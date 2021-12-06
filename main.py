7d4d6176bdb0abfb4cc4fbfd37be5de275f33e8a
Daniel <dtaylor6@umbc.edu>
2021-12-06 16:39:41 -0500

    bug fix

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ class Enemy(Ship):
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
-    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) is not None
+    return obj1.mask.overlap(obj2.mask, (int(offset_x), int(offset_y))) is not None
 
 
 def game_main(difficulty):
